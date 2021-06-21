63cfe3cffbe4decc0f60796f202fdf45756ef0e8
CodeIsJustLikeMagic <janelle.pfeifer@freenet.de>
2021-06-22 01:48:13 +0200

    Fixed auto run. Fixed button press being registered multiple times. Prettier state machine.

--- sensor_box.py
+++ sensor_box.py
@@ -1,71 +1,115 @@
+#! /usr/bin/env python3
+
+from time import sleep
+# sleep(120)
+
+import csv
+import threading
 from pitop.pma import SoundSensor
 from pitop.pma import LightSensor
 from pitop.miniscreen import UpButton, DownButton, SelectButton, CancelButton
-from time import sleep
-import csv
-import threading
 from pitop.miniscreen import OLED
 import datetime
+import logging
+import sys, errno
 
-print("sensor box scrip running")
+logging.basicConfig(handlers=[logging.StreamHandler(stream=sys.stdout)]
+                    , format="%(asctime)s %(name)s:%(levelname)s:%(message)s", datefmt="%F %A %T", level=logging.DEBUG)
+# logging.FileHandler(filename="sensor_box.log", encoding='utf-8',mode = "a+")
+logging.debug("sensor box scrip running")
+print("before initialization")
+try:
+
+    logging.debug(f"try to initialize OLED and Sensors")
+
+    mini_screen = OLED()
+    mini_screen.set_max_fps(1)
+    mini_screen.draw_multiline_text("welcome to sensor box script. Press o to start recording enviroment", font_size=12)
+
+except Exception as e:
+    logging.exception("kaputt")
+sys.exit(errno.EAGAIN)
+
 sound_sensor = SoundSensor("A1")
 light_sensor = LightSensor("A0")
-
-mini_screen = OLED()
-mini_screen.set_max_fps(1)
-mini_screen.draw_multiline_text("welcome to sensor box script. Press o to start recording enviroment", font_size = 12)
-
-up = UpButton()
-down = DownButton()
 select = SelectButton()
 cancel = CancelButton()
 
+logging.debug("starting idle phase")
 recording = False
 
+
 def readData():
-    with open('sensor_'+str(datetime.datetime.now())+".csv", 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(["time", "sound_sensor","light_sensor"])
+    logging.debug("start recording")
+    global recording
+    with open('sensor_' + str(datetime.datetime.now()) + ".csv", 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(["time", "sound_sensor", "light_sensor"])
         while recording:
             writer.writerow([datetime.datetime.now(), sound_sensor.reading, light_sensor.reading])
-            print("add line to csv")
+            # print("add line to csv")
             sleep(0.1)
 
+
 def start_recording():
-    mini_screen.draw_multiline_text("----- recording ------ Press x to stop recording.", font_size = 12)
+    mini_screen.draw_multiline_text("----- recording ------ Press x to stop recording.", font_size=12)
+    global recording
     recording = True
-    t = threading.Thread(name = 'readData', target = readData)
+    t = threading.Thread(name='readData', target=readData)
     t.setDaemon(True)
     t.start()
 
+
 def stop_recording():
-    mini_screen.draw_multiline_text("stopped recording. Press o to start recording.", font_size = 12)
+    logging.debug("stop recording")
+    mini_screen.draw_multiline_text("stopped recording. Press o to start recording.", font_size=12)
+    global recording
     recording = False
 
-exiting = False
+
+cancel_pressed = False
+select_pressed = False
 
 
-while True:
-    if cancel.is_pressed:
-        print("cancel_pressed")
-        if recording:#press cancel once (from running state)
-            recording = False
-            stop_recording()
-            print("Cancel pressed")
+def select_down():
+    global select_pressed
+    if select_pressed != select.is_pressed:  # state has canged
+        select_pressed = select.is_pressed
+        if select.is_pressed:
+            return True
         else:
-            if exiting:# press cancel three times (from stopped state)
-                exit()
-            else:# press cancel twice (from stopped state)
-                mini_screen.draw_multiline_text("are you sure you want to exit? Press x again to confirm.", font_size = 12)
-                exiting = True
-        sleep(1)
-    if select.is_pressed:
-        print("select_pressed")
-        if exiting:# if we were in cancel twice state go back to cancel once state
-            exiting = False
-            stop_recording()
-        elif not recording:# enter recording state from from cancel once state
-            recording = True
+            return False
+
+
+def cancel_down():
+    global cancel_pressed
+    if cancel_pressed != cancel.is_pressed:  # state has canged
+        cancel_pressed = cancel.is_pressed
+        if cancel.is_pressed:
+            return True
+        else:
+            return False
+
+
+state = 0
+while True:
+
+    if state == 0:  # idle (not recording) state
+        if select_down():
+            state = 1
             start_recording()
-            print("Select pressed")
-        sleep(1)
+        elif cancel_down():
+            state = 2
+            mini_screen.draw_multiline_text("Are you sure you want to exit? Press x again to confirm.", font_size=12)
+    if state == 1:  # recording state
+        if select_down():
+            pass
+        elif cancel_down():
+            state = 0
+            stop_recording()
+    if state == 2:  # are you sure you want to exit state
+        if select_down():
+            state = 0
+            stop_recording()
+        elif cancel_down():
+            exit()
