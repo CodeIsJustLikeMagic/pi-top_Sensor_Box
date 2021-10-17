30ff24d2c00b01e0f04d939e126ec76ae1bc58bf
CodeIsJustLikeMagic <janelle.pfeifer@freenet.de>
2021-10-17 23:11:24 +0200

    reading pi camera into cv2

--- pi_camera_test.py
+++ pi_camera_test.py
@@ -1,7 +1,10 @@
 from picamera import PiCamera
+from picamera.array import PiRGBArray
 import time
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt
+
 
 def contrast(img_hsv):
     # michelson contrast
@@ -20,8 +23,53 @@ def brightness(img_hsv):
     h, s, v = cv2.split(img_hsv)
     print("brightness:", np.mean(v))
 
+
+def color_distribution(img_hsv):
+    pass
+    # most common color values ?
+    # do white and black count ?
+
+
+def histogramms(img_hsv):
+    print("hue")
+    histr_hue1 = cv2.calcHist(img_hsv, [0], None, [256], [0, 256])
+    plt.plot(histr_hue1)
+    plt.show()
+
+    print("saturation")
+    histr_hue2 = cv2.calcHist(img_hsv, [1], None, [256], [0, 256])
+    plt.plot(histr_hue2)
+    plt.show()
+
+    print("value")
+    histr_hue3 = cv2.calcHist(img_hsv, [2], None, [256], [0, 256])
+    plt.plot(histr_hue3)
+    plt.show()
+
+
+def do_the_thing(img):
+    # create histogramm. should later be written in csv file
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    contrast(img_hsv)
+    brightness(img_hsv)
+    # histogramms(img_hsv)
+    # threshholds(img_hsv)
+    # colored_scatter_plot(img, img_hsv)
+    # contrast
+    # bightness
+    # color distribution
+
+
 camera = PiCamera()
-camera.resolution(1024, 768)
-camera.start_preview()
-time.sleep(2)
-camera.capture('foo.jpg')
+camera.resolution = (640, 480)
+rawCapture = PiRGBArray(camera, size=(640, 480))
+
+time.sleep(0.1)
+camera.capture(rawCapture, format="bgr")
+img = rawCapture.array
+
+do_the_thing(img)
+camera.capture("foo.jpg")
+
+cv2.imshow("image", img)
+cv2.waitKey(0)
