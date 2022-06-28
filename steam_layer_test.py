3f0d1290575c74c4734986fb904078186c25f212
CodeIsJustLikeMagic <janelle.pfeifer@freenet.de>
2022-06-28 19:38:16 +0200

    replace dummy data with light and sound sensor data

--- steam_layer_test.py
+++ steam_layer_test.py
@@ -2,10 +2,16 @@ from pylsl import StreamInfo, StreamOutlet, local_clock
 from random import random as rand
 import time
 
+from pitop import SoundSensor
+from pitop import LightSensor
+
+sound_sensor = SoundSensor("A1")
+light_sensor = LightSensor("A0")
+
 srate = 100
 name = 'pi-top chan'
-ttype = 'Thermal'
-n_channels = 8
+ttype = 'LightSound'
+n_channels = 3
 info = StreamInfo(name, ttype, n_channels, srate, 'float32', 'myuid34234')
 outlet = StreamOutlet(info)
 
@@ -19,8 +25,9 @@ while True:
         # make a new random n_channels sample; this is converted into a
         # pylsl.vectorf (the data type that is expected by push_sample)
         # now send it
-        mysample = [rand() for _ in range(n_channels)]
+        # mysample = [rand() for _ in range(n_channels)]
+        mysample = [sound_sensor.reading, light_sensor.reading]
         outlet.push_sample(mysample)
     sent_samples += required_samples
     # now send it and wait for a bit before trying again.
-    time.sleep(0.01)
+    time.sleep(0.1)
